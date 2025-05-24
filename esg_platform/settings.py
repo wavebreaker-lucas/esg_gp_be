667c705d6c94cc3a554ff9f8c6ee2ee71aa9e67c
Goma <info@wavebreakerlabs.com>
2025-05-25 04:15:27 +0800

    Remove CompressedManifestStaticFilesStorage setting from settings.py to streamline static files configuration

--- esg_platform/settings.py
+++ esg_platform/settings.py
@@ -182,7 +182,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (Uploads)
